8502c61887788a0e31250d557ef64a1dcaaec1bd
Maram Naqeeb <maramnaqib1998@gmail.com>
2023-10-19 14:35:05 +0300

    candidate bestPractice

--- candidate_bestPractice_OrangeHRM/cypress/e2e/OrangeHRM/candidate.spec.cy.ts
+++ candidate_bestPractice_OrangeHRM/cypress/e2e/OrangeHRM/candidate.spec.cy.ts
@@ -4,16 +4,18 @@ import candidate from "../../support/projectObject/candidate";
 const candidateObj: candidate = new candidate();
 
 describe("OrangeHRM-create and edit vacancy", () => {
-  
   beforeEach(function () {
     cy.visit("/");
     candidateObj.loginFUNC("Admin", "admin123");
-    apiHelpers.addCandidate()
+    apiHelpers.addCandidate();
+  });
 
+  afterEach(function () {
+    apiHelpers.deleteCandidate();
   });
 
   it("TCs2: OrangeHRM-edit vacancy", () => {
-    apiHelpers.stateShortlistStatus()
-    candidateObj.scheduleInterview()
+    apiHelpers.stateShortlistStatus();
+    candidateObj.scheduleInterview();
   });
 });
---
+++ candidate_bestPractice_OrangeHRM/cypress/support/API/payload/userPayload.ts
@@ -0,0 +1,8 @@
+export interface userPayload {
+    empNumber: number;
+    password: string;
+    status: boolean;
+    userRoleId: number;
+    username: string;
+  }
+  
